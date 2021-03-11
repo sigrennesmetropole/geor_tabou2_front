0d04456caebd1b0183dd8a8e137eea13fd6fde60
gaetan <gaetan.bruel@jdev.fr>
2021-03-11 18:32:38 +0100

    Util method to create uniq ID

---
+++ js/extension/utils/common.js
@@ -0,0 +1,8 @@
+export function makeId() {
+    let ID = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for ( let i = 0; i < 12; i++ ) {
+        ID += characters.charAt(Math.floor(Math.random() * 36));
+    }
+    return ID;
+}
