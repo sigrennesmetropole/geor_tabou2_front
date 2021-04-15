95c09ec5eae779fa14dbeafbdc71bc4b182bc87b
gaetanbrl <gaetan.bruel@jdev.fr>
2021-04-15 12:04:56 +0200

    Change default mapViewer by local config

--- js/app.jsx
+++ js/app.jsx
@@ -5,9 +5,10 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-import { checkForMissingPlugins } from '@mapstore/utils/DebugUtils';
+ import { checkForMissingPlugins } from '@mapstore/utils/DebugUtils';
 import main from '@mapstore/product/main';
+import MapViewer from "@mapstore/product/pages/MapViewer";
+
 const ConfigUtils = require('@mapstore/utils/ConfigUtils').default;
 /**
  * Add custom (overriding) translations with:
@@ -39,7 +40,26 @@ ConfigUtils.setLocalConfigurationFile('localConfig.json');
  *     }]
  * });
  */
-const appConfig = require('@mapstore/product/appConfig').default;
+//const appConfig = require('@mapstore/product/appConfig').default;
+
+const cfg = require('@mapstore/product/appConfig').default;
+
+const appConfig = {
+    ...cfg,
+    pages: [
+        {
+            name: "mapviewer",
+            path: "/",
+            component: MapViewer
+        },
+        {
+            name: "mapviewer",
+            path: "/viewer/:mapType/:mapId",
+            component: MapViewer
+        }
+    ],
+    appEpics: {}
+};
 
 /**
  * Define a custom list of plugins with:
