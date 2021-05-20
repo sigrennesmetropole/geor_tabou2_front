89743dc049e4c35deaeaea41a470b7de745c19e5
gaetanbrl <gaetan.bruel@jdev.fr>
2021-05-20 12:26:35 +0200

    Display and allow to change gouv accordions bloc

--- js/extension/components/form/identify/Tabou2GouvernanceAccord.jsx
+++ js/extension/components/form/identify/Tabou2GouvernanceAccord.jsx
@@ -1,85 +1,163 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from "react";
+import { isEmpty, isEqual, pick, has, get, zipObject, keys } from "lodash";
 import { Checkbox, Col, Row, FormGroup, FormControl, Grid, ControlLabel } from "react-bootstrap";
-import { isEmpty } from "lodash";
+import Tabou2Combo from '@ext/components/form/Tabou2Combo';
+import { getRequestApi } from "@ext/api/search";
+import { Multiselect } from "react-widgets";
+import "@ext/css/identify.css";
 
-export default function Tabou2GouvernanceAccord({ layer, ...props }) {
-    const fields = [
-        {
-            name: "promoteur",
-            fieldApi: "promoteur",
-            label: "Promoteur",
-            api: "/",
-            type: "text",
-            layers: ["layerPA"]
-        }, {
-            name: "maitreoeuvre",
-            fieldApi: "maitreOeuvre.libelle",
-            label: "Maître d\'oeuvre",
-            api: "/",
-            type: "text",
-            layers: [],
-            write: false
-        }, {
-            name: "decision",
-            fieldApi: "desision.libelle",
-            label: "Décision",
-            api: "/",
-            type: "text",
-            layers: ["layerOA", "layerSA"]
-        }, {
-            name: "maitriseouvrage",
-            fieldApi: "maitriseOuvrage.libelle",
-            label: "Maîtrise d'Ouvrage",
-            api: "/",
-            type: "text",
-            layers: []
-        }, {
-            name: "modeamenagement",
-            fieldApi: "modeAmenagement.libelle",
-            label: "Mode d'aménagement",
-            api: "/",
-            type: "text",
-            layers: ["layerOA", "layerSA"]
-        }, {
-            name: "amenageur",
-            fieldApi: "amenageur.libelle",
-            label: "Aménageur",
-            api: "/",
-            type: "text",
-            layers: ["layerOA", "layerSA"],
-            write: false
+export default function Tabou2GouvernanceAccord({ initialItem, programme, operation, mapFeature, ...props }) {
+    let layer = props?.selection?.layer;
+
+    const [values, setValues] = useState({});
+    const [fields, setFields] = useState([]);
+    const [required, setRequired] = useState({});
+    const getFields = () => [{
+        name: "promoteur",
+        label: "Promoteur",
+        type: "multi",
+        data: props.tiers.filter(t => t.libelle === "Maître d'ouvrage").map(t => t.nom),
+        readOnly: true
+    }, {
+        name: "decision",
+        label: "Décision",
+        field: "decision.libelle",
+        layers:["layerSA", "layerOA"],
+        type: "combo",
+        apiLabel: "libelle",
+        api: "decisions",
+        placeholder: "Décisions...",
+        source: operation,
+        readOnly: false
+    }, {
+        name: "maitriseOuvrage",
+        label: "Maîtrise d'ouvrage",
+        field: "maitriseOuvrage.libelle",
+        layers:["layerSA", "layerOA"],
+        type: "combo",
+        apiLabel: "libelle",
+        api: "maitrise-ouvrage",
+        placeholder: "Maîtrise d'ouvrage...",
+        source: operation,
+        readOnly: false
+    }, {
+        layers:["layerSA", "layerOA"],
+        name: "modeAmenagement",
+        label: "Mode d'aménagement",
+        field: "modeAmenagement.libelle",
+        type: "combo",
+        apiLabel: "libelle",
+        api: "mode-amenagement",
+        placeholder: "Mode d'aménagement...",
+        source: initialItem,
+        readOnly: false
+    }, {
+        name: "amenageur",
+        field: "nom",
+        label: "Aménageurs",
+        layers:["layerSA", "layerOA"],
+        type: "multi",
+        data: props.tiers.filter(t => t.libelle === "Maître d'ouvrage").map(t => t.nom),
+        readOnly: true
+    },{
+        name: "moe",
+        label: "Maîtrise d'oeuvre",
+        type: "multi",
+        data: props.tiers.filter(t => t.libelle === "Maître d'oeuvre").map(t => t.nom),
+        readOnly: true
+    }].filter(el => el?.layers?.includes(layer) || !el?.layers);
+
+    /**
+     * Effect
+     */
+    // return writable fields as object-keys
+
+    useEffect(() => {
+        const calculFields = getFields();
+        const mandatoryFields = calculFields.filter(f => f.require).map(f => f.name);
+        if (!isEqual(initialItem, values)) {
+            setValues(initialItem);
+            setFields(calculFields);
+            setRequired(mandatoryFields);
         }
-    ];
+    }, [initialItem]);
 
+    const getValue = (item) => {
+        if (isEmpty(values) || isEmpty(operation)) return null;
+        let itemSrc = getFields().filter(f => f.name === item.name)[0]?.source;
+        console.log(itemSrc);
+        return get(itemSrc, item?.field);
+    }
+
+    const changeInfos = (item) => {
+        let newValues = {...values, ...item};
+        setValues(newValues);
+        // send to parent to save
+        let accordValues = pick(newValues, getFields().filter(f => !f.readOnly).map(f => f.name));
+        props.change(accordValues, pick(accordValues, required));
+    }
+
+    /**
+     * COMPONENT
+     */
     const marginTop = "10px";
     return (
         <Grid style={{ width: "100%" }} className={""}>
             {
-                fields.filter(f => isEmpty(f.layers) || f?.layers.indexOf(layer) > -1).map(field => (
-                    <Row style={{ marginTop: marginTop }} key={`key-formrow-${field.name}`}>
-                        <Col xs={12}>
-                            <FormGroup key={`key-formgp-${field.name}`}>
-                                {
-                                    field.type !== "boolean" ? <ControlLabel>{field.label}</ControlLabel> : null
-                                }
-                                {
-                                    field.type === "boolean" ?
-                                        (<Checkbox inline="true" key={`key-chbox-${field.name}`} className="col-xs-3">{field.label}</Checkbox>) : null
-
-                                }
-                                {
-                                    field.type !== "boolean" ?
-                                        (<FormControl
-                                            type="text"
-                                            key={`key-ctrl-${field.name}`}
-                                            placeholder={field.label} />) : null
-                                }
-                            </FormGroup>
-                        </Col>
-                    </Row>
+                fields.filter(f => isEmpty(f.layers) || f?.layers.indexOf(layer) > -1).map(item => (
+                    <>
+                        {
+                            item.type !== "boolean" ? <ControlLabel>{item.label}</ControlLabel> :  null
+                        }
+                        {
+                            item.type === "boolean" ?
+                                (<Checkbox 
+                                    inline="true"
+                                    checked={item.value(item) || false}
+                                    disabled={item.readOnly}
+                                    id={`chbox-${item.name}`}
+                                    className="col-xs-5">
+                                    <ControlLabel>{item.label}</ControlLabel>
+                                </Checkbox>) : null
+                        }{
+                            item.type === "text" ?
+                                (<FormControl 
+                                    placeholder={item.label}
+                                    value={getValue(item) || ""}
+                                    readOnly={item.readOnly}
+                                    onChange={(v) => changeInfos({[item.name]: v.target.value})}
+                                />) : null
+                        }{
+                            item.type === "combo" ? (
+                                <Tabou2Combo
+                                    load={() => getRequestApi(item.api, props.pluginCfg.apiCfg, {})}
+                                    disabled={item?.readOnly || false}
+                                    placeholder={item?.placeholder || ""}
+                                    filter="contains"
+                                    textField={item.apiLabel}
+                                    onLoad={(r) => r?.elements || r}
+                                    name={item.name}
+                                    value={get(values, item.name)}
+                                    onSelect={(v) => changeInfos({[item.name]: v})}
+                                    onChange={(v) => !v ? changeInfos({[item.name]: v}) : null}
+                                    messages={{
+                                        emptyList: 'La liste est vide.',
+                                        openCombobox: 'Ouvrir la liste'
+                                    }}
+                                />
+                            ) : null
+                        }{
+                            item.type === "multi" ? (
+                                <Multiselect
+                                    readOnly={item.readOnly}
+                                    value={item.data}
+                                    className={ item.readOnly ? "tagColor noClick" : "tagColor"}
+                                />
+                            ) : null
+                        }
+                    </>
                 ))
             }
-
         </Grid>
     );
 }
