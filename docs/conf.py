285e63a6110e9ad3947cce24f3dedf457403228d
gaetanbrl <gaetan.bruel@jdev.fr>
2021-06-08 11:00:41 +0200

    Change master doc and root file

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,8 @@ author = 'sigrennesmetropole gaetanbrl'
 
 
 # -- General configuration ---------------------------------------------------
-
+master_doc = 'index'
+source_suffix = '.rst'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
