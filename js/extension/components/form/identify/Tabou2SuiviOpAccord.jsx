fc38bf27e19b858359c8a3411292531ddb2c45dd
LLE23797 <lisa.le_goff__mauvoisin@open-groupe.com>
2024-05-16 15:13:40 +0200

    CDCRM-1185 Modif emplacement champs

--- js/extension/components/form/identify/Tabou2SuiviOpAccord.jsx
+++ js/extension/components/form/identify/Tabou2SuiviOpAccord.jsx
@@ -22,6 +22,7 @@ const avoidReRender = (prevProps, nextProps) => {
 const Tabou2SuiviOpAccord = ({
     initialItem,
     operation,
+    programme,
     layer,
     authent,
     change = () => { },
